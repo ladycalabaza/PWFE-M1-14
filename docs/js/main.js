5921c91a2b264c844747f943fcb299abf66547a2
ladycalabaza <jessolivera25@gmail.com>
2019-12-04 21:12:14 -0300

    Implementando ajax y json

---
+++ docs/js/main.js
@@ -0,0 +1,17 @@
+//Ambito: linea de ejecucion principal
+let titulo = "Comenzó la venta de entradas para el Mundial de Rusia 2018"
+let autor  = "por Laura Smasnatta"
+let detalle = "Hoy se abrió la venta al público de tickets, a través del sitio oficial de la FIFA. El proceso se dividirá en dos partes y los espectadores deberán solicitar una tarjeta de hincha 'Fan ID'"
+let imagen = "tickets-rusia-mundial.jpg"
+
+crearNoticia(titulo, autor, detalle, imagen)
+
+//Ambito: linea de ejcucion funcional
+function crearNoticia ( t, a , d, i){
+
+       let plantilla = document.querySelector ("ultimas-noticias article").cloneNode(true)
+
+       console.log (x)
+
+
+    }
